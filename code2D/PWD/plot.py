9117a8faabfb2627772f645d8ef654b3d7bb1506
Daryubel <he19991017@hotmail.com>
2023-04-18 10:28:30 +0800

    Fixed an issue in residual yielding

--- code2D/PWD/plot.py
+++ code2D/PWD/plot.py
@@ -1,6 +1,3 @@
-# read PWDF.dat and imshow it
-
-
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
@@ -8,61 +5,20 @@ import matplotlib.cm as cm
 plt.rc('font', family='Times New Roman', size=14)
 
 path = 'PWD/'
-# model = 'SLMO'
-# model = 'DAT0003'
-# model = 'ladder'
-# model = 'SLMO_ROUGH'
 # model = 'SLOL'
-# model = 'SLOL_NL'
-model = 'SLOLargeNL'
+# model = 'ladder'
+model = 'ladderLarge'
 
-dipFile = path+'dip_'+model+'.dat'
-orgFile = path+'org_'+model+'.dat'
-
+dipFile = path+model+'_dip.dat'
+pwdFile = path+model+'_PWD.dat'
 
 dip = (np.loadtxt(dipFile))
 print(dip.shape)
-sec = np.transpose((np.loadtxt(orgFile)))
-print(sec.shape)
+pwd = (np.loadtxt(pwdFile))
+print(pwd.shape)
 
-# abort if the two files are not the same size
-if dip.shape != sec.shape:
-    print('Dimension mismatch error!')
-    exit()
-
-plt.figure(1, figsize=(10,6))
-plt.subplot(1, 3, 1)
-plt.imshow(sec, cmap=cm.gray)
-# plt.title("Original Trace")
-plt.xlabel('Trace Number')
-plt.ylabel('Time (ns)')
-# plt.colorbar().set_label(r'$E_z \quad (V/m)$')
-
-
-threshold = 1.2
-for i in range(len(dip)):
-    for j in range(len(dip[0])):
-        if np.abs(dip[i][j]) < threshold:
-            sec[i][j] = sec[i][j] * (0 * 10**(-3))
-
-
-# plot original trace and secTemp trace
-plt.subplot(1, 3, 2)
-plt.imshow(dip, cmap=cm.seismic)
-# plt.title("Dip")
-plt.xlabel('Trace Number')
-plt.ylabel('Time (ns)')
-# plt.colorbar().set_label('Dip (Rad)')
-plt.subplot(1, 3, 3)
-plt.imshow(sec, cmap=cm.gray)
-# plt.title("Trace with PWD")
-plt.xlabel('Trace Number')
-plt.ylabel('Time (ns)')
-# plt.colorbar().set_label(r'$E_z \quad (V/m)$')
-plt.savefig(path+'PWD_'+model+'.png', dpi=600)
-
-plt.figure(2)
-plt.imshow(sec, cmap=cm.gray, aspect='auto')
+plt.figure(1)
+plt.imshow(pwd, cmap=cm.gray, aspect='auto')
 # plt.title("Trace with PWD")
 plt.xlabel('Trace Number')
 plt.ylabel('Time (ns)')
@@ -72,10 +28,10 @@ ax.spines['right'].set_linewidth(2)
 ax.spines['top'].set_linewidth(2)
 ax.spines['left'].set_linewidth(2)
 ax.spines['bottom'].set_linewidth(2)
-plt.savefig(path+'PWD_'+model+'_sec.png', dpi=600)
+plt.savefig(path+'r1_PWD_'+model+'_sec.png', dpi=600)
 
-plt.figure(3)
-plt.imshow(dip, cmap=cm.seismic, aspect='auto')
+plt.figure(2)
+plt.imshow(dip, cmap=cm.jet, aspect='auto')
 # plt.title("Dip")
 plt.xlabel('Trace Number')
 plt.ylabel('Time (ns)')
@@ -85,7 +41,7 @@ ax.spines['right'].set_linewidth(2)
 ax.spines['top'].set_linewidth(2)
 ax.spines['left'].set_linewidth(2)
 ax.spines['bottom'].set_linewidth(2)
-plt.savefig(path+'PWD_'+model+'_dip.png', dpi=600)
+plt.savefig(path+'r1_PWD_'+model+'_dip.png', dpi=600)
 
 
 plt.show()
