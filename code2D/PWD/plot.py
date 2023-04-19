b9bee2934eaaf058b0d7d4a07edc80a4558c82fe
Daryubel <he19991017@hotmail.com>
2023-04-19 11:06:17 +0800

    Updated codes

--- code2D/PWD/plot.py
+++ code2D/PWD/plot.py
@@ -12,12 +12,13 @@ model = 'ladderLarge'
 
 dipFile = path+model+'_dip.dat'
 pwdFile = path+model+'_PWD.dat'
+orgFile = path+model+'_org.dat'
 
 dip = (np.loadtxt(dipFile))
 print(dip.shape)
 pwd = (np.loadtxt(pwdFile))
 print(pwd.shape)
-
+org = (np.loadtxt(orgFile))
 
 # # Get the jet colormap
 # cmap = plt.cm.jet
@@ -58,5 +59,17 @@ ax.spines['left'].set_linewidth(2)
 ax.spines['bottom'].set_linewidth(2)
 plt.savefig(path+'r1_PWD_'+model+'_dip.png', dpi=600)
 
+plt.figure(3)
+plt.imshow(org, cmap=cm.gray, aspect='auto')
+# plt.title("Original Trace")
+plt.xlabel('Trace Number')
+plt.ylabel('Time (ns)')
+plt.colorbar().set_label(r'$E_z \quad (V/m)$')
+ax = plt.gca()
+ax.spines['right'].set_linewidth(2)
+ax.spines['top'].set_linewidth(2)
+ax.spines['left'].set_linewidth(2)
+ax.spines['bottom'].set_linewidth(2)
+# plt.savefig(path+'r1_PWD_'+model+'_org.png', dpi=600)
 
 plt.show()
